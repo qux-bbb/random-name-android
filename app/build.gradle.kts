cbf5378fb274904eaaf8828914e9094c6483d971
qux <1147635419@qq.com>
2026-07-12 21:18:33 +0800

    版本号改为 0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hello.randomname"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 1
+        versionName = "0.1"
     }
 
     buildFeatures {
