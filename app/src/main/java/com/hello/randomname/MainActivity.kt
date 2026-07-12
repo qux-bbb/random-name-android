b5ee4922441b1fba1e93fabc0bffb8e389880afa
qux <1147635419@qq.com>
2026-07-12 16:55:20 +0800

    添加 GPLv3 许可证

--- app/src/main/java/com/hello/randomname/MainActivity.kt
+++ app/src/main/java/com/hello/randomname/MainActivity.kt
@@ -1,3 +1,21 @@
+/*
+ * 随机姓名 Random Name - 随机中文姓名生成器
+ * Copyright (C) 2026
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.hello.randomname
 
 import android.app.AlertDialog
